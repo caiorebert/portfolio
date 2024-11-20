f7ab95eb45fdd7f44bf045503b7ad20cacd497f1
caio.rebert <caio.rebert@esig.com.br>
2024-11-20 00:40:42 -0300

    first commit

---
+++ src/app/data.ts
@@ -0,0 +1,158 @@
+import exp from "constants";
+
+const informacoes = {
+    experiencias: [
+        {
+            empresa: 'Wilivro',
+            cargo: 'Assistente de Programador',
+            inicio: '02/08/2021',
+            fim: '17/01/2022',
+            periodo: '02/08/2021 - 17/01/2022',
+            local: 'Não especificado',
+            conteudo: 'Não especificado',
+            linguagens: [
+                'PHP',
+                'JavaScript'
+            ],
+            frameworks: [
+                'Yii Framework'
+            ],
+            bancos: [
+                'MySQL'
+            ]
+        },
+        {
+            empresa: 'Esig Software e Consultoria',
+            
+            cargo: 'Analista de Sistemas I - A',
+            inicio: '24/01/2022',
+            fim: 'Trabalho atual',
+            periodo: '24/01/2022 - Trabalho atual',
+            local: 'Não especificado',
+            conteudo: 'Não especificado',
+            linguagens: [
+                'PHP',
+                'JavaScript',
+                'C#',
+                'Java'
+            ],
+            frameworks: [
+                'Laravel',
+                '.NET Framework',
+                'VueJS',
+                'Spring Boot'
+            ],
+            bancos: [
+                'Oracle',
+                'PostgreSQL'
+            ]
+        }
+    ],
+    formacoes: [
+        {
+            instituicao: 'Universidade Federal do Rio Grande do Norte',
+            curso: 'Bacharelado em Tecnologia da Informação',
+            inicio: '2021',
+            fim: '2025',
+            periodo: '2017 - 2022',
+            local: 'Natal, RN',
+            conteudo: 'Não especificado'
+        },
+        {
+            instituicao: 'Universidade Federal do Rio Grande do Norte',
+            curso: 'Técnico em Informática para Internet',
+            inicio: '2019',
+            fim: '2021',
+            periodo: '2019 - 2021',
+            local: 'Natal, RN',
+            conteudo: 'Não especificado'
+        }
+    ],
+    stacks: [
+        {
+            nome: 'PHP',
+            experiencia: '+4 anos de experiência',
+            frameworks: [
+                'Laravel',
+                'Yii Framework',
+                'CodeIgniter'
+            ]
+        },
+        {
+            nome: 'JavaScript',
+            experiencia: '+4 anos de experiência',
+        },
+        {
+            nome: "VueJS",
+            experiencia: "+-3 anos de experiência",
+            frameworks: [
+                'NuxtJS'
+            ]
+        },
+        {
+            nome: 'C#',
+            experiencia: '+-3 anos de experiência',
+            frameworks: [
+                '.NET Framework',
+                'ASP.NET Core'
+            ]
+        },
+        {
+            nome: 'Java',
+            experiencia: '+1 ano de experiência',
+            frameworks: [
+                'Spring Boot'
+            ]
+        },
+        {
+            nome: "Flutter",
+            experiencia: "Apenas estudos avulsos"
+        },
+        {
+            nome: "ReactJS",
+            experiencia: "Apenas estudos avulsos",
+            frameworks: [
+                'NextJS'
+            ]
+        },
+        {
+            nome: "NodeJS",
+            experiencia: "Apenas estudos avulsos",
+            frameworks: [
+                'NestJS'
+            ]
+        }
+    ],
+    projetos: [
+        {
+            nome: "ArezDelivery",
+            descricao: `Sistema de delivery de comida para a minha cidade natal, Arez/RN. 
+                        Por enquanto ainda está em desenvolvimento.`,
+            tecnologias: [
+                'NestJS',
+                'NextJS',
+                'Postgres'
+            ],
+            link: [
+                'https://arezdelivery-front.vercel.app/'
+            ]
+        },
+        {
+            nome: "Send2You",
+            descricao: `Sistema de envio de mensagens para outras pessoas, com o uso de
+                        SocketIO possibilitando a comunicacao em tempo real.`,
+            tecnologias: [
+                'Flutter',
+                'Express',
+                'Postgres',
+                'SocketIO'
+            ],
+            link: [
+                'https://github.com/caiorebert/send2you',
+                'https://github.com/caiorebert/send2you-api'
+            ]
+        }
+    ]
+}
+
+export default informacoes;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,384 @@
+"use client";
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Accordion, AccordionDetails, AccordionSummary, Collapse, Grid2, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Paper, Typography } from '@mui/material'
+import perfil from '../../public/fotos/perfil.jpeg'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SendIcon from '@mui/icons-material/Send';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import InboxIcon from '@mui/icons-material/Inbox';
+import Extension from '@mui/icons-material/Extension';
+import ArrowRight from '@mui/icons-material/ArrowRight';
+import Storage from '@mui/icons-material/Storage';
+import { ExpandLess, ExpandMore, Language, StarBorder } from '@mui/icons-material';
+import { useState } from 'react';
+import informacoes from './data';
 
 export default function Home() {
+  const [open, setOpen] = useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Grid2 container size={12}>
+      <Grid2 container size={12} height={200} bgcolor={'#090f13'}>
+        
+      </Grid2>
+      <Grid2 container size={12} bgcolor={'#171f25'}>
+        <Grid2 size={{ xs:12, md: 2 }} className={styles.gridFoto}>
+          <Paper
+            elevation={3}
+            style={{
+              backgroundColor: '#171f25',
+            }}
           >
-            By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
+              style={{marginTop: -100, borderRadius: 20, paddingBottom: 10}}
+              src={perfil}
+              alt="Perfil"
+              width={200}
+              height={250} />
+          </Paper>
+        </Grid2>
+        <Grid2 size={{ xs:12, md: 4 }} className={styles.gridTextoFoto}>
+          <h1>
+            <strong>Caio Rebert</strong><br></br>
+          </h1>
+          <h2 style={{marginTop: 10}}>
+            <small>Desenvolvedor Full Stack</small>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+        </Grid2>
+        <Grid2 size={{ xs:12, md: 4 }} className={styles.gridTextoFoto}>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            <b>Idade:</b> 22 anos
           </p>
-        </a>
-      </div>
-    </main>
+          <p>
+            <b>Email:</b> caiorebert070802.cr@gmail.com
+          </p>
+        </Grid2>
+      </Grid2>
+      <br></br>
+      <Grid2 size={12} padding={1} bgcolor={'#090f13'} color={'white'} paddingTop={5}>
+        <Grid2 size={12} textAlign={'center'}>
+          <Typography variant='h4'>Experiências Profissionais</Typography>
+        </Grid2>
+        <Grid2 container size={12} spacing={2} paddingBottom={10} marginTop={5}>
+          {
+            informacoes.experiencias.map((experiencia, index) => (
+              <Grid2 key={index} size={{ xs: 12, md: 4 }} padding={1}>
+                <Accordion
+                  style={{
+                    backgroundColor: '#506ee5',
+                    border: '3px solid #506ee5',
+                    color: 'white'
+                  }}>
+                  <AccordionSummary
+                    style={{ fontSize: 20 }}
+                    aria-controls={`panel${index}-content`}
+                    id={`panel${index}-header`}
+                    expandIcon={<ExpandMoreIcon style={{ color: 'white' }} />}
+                  >
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td colSpan={2}>
+                            <h4>{experiencia.cargo} | {experiencia.empresa}</h4>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <small>Início: {experiencia.inicio}</small>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <small>Fim: {experiencia.fim}</small>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </AccordionSummary>
+                  <AccordionDetails style={{ padding: 10, borderTop: '1px solid white' }}>
+                    <List sx={{ width: '100%', maxWidth: 360 }} component="nav">
+                      <ListItemButton>
+                        <ListItemIcon>
+                          <Language style={{color:'white'}} />
+                        </ListItemIcon>
+                        <ListItemText primary="Linguagens de programação" />
+                      </ListItemButton>
+                      <List component="div" disablePadding>
+                        {experiencia.linguagens.map((linguagem, idx) => (
+                          <ListItemButton key={idx} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                              <ArrowRight style={{ color: 'white' }} />
+                            </ListItemIcon>
+                            <ListItemText primary={linguagem} />
+                          </ListItemButton>
+                        ))}
+                      </List>
+                      <ListItemButton>
+                        <ListItemIcon>
+                          <Extension style={{color:'white'}}/>
+                        </ListItemIcon>
+                        <ListItemText primary="Frameworks" />
+                      </ListItemButton>
+                      <List component="div" disablePadding>
+                        {experiencia.frameworks.map((framework, idx) => (
+                          <ListItemButton key={idx} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                              <ArrowRight style={{ color: 'white' }} />
+                            </ListItemIcon>
+                            <ListItemText primary={framework} />
+                          </ListItemButton>
+                        ))}
+                      </List>
+                      <ListItemButton>
+                        <ListItemIcon>
+                          <Storage style={{color: 'white'}} />
+                        </ListItemIcon>
+                        <ListItemText primary="Bancos de dados" />
+                      </ListItemButton>
+                      <List component="div" disablePadding>
+                        {experiencia.bancos.map((banco, idx) => (
+                          <ListItemButton key={idx} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                              <ArrowRight style={{ color: 'white' }} />
+                            </ListItemIcon>
+                            <ListItemText primary={banco} />
+                          </ListItemButton>
+                        ))}
+                      </List>
+                    </List>
+                  </AccordionDetails>
+                </Accordion>
+              </Grid2>
+            ))
+          }
+        </Grid2>
+        <Grid2 size={12} textAlign={'center'}>
+          <Typography variant='h4'>Stacks conhecidas</Typography>
+        </Grid2>
+        <Grid2 container size={12} spacing={2} paddingBottom={10} marginTop={5}>
+          {
+            informacoes.stacks.map((stack, index) => (
+              <Grid2 key={index} size={{ xs: 12, md: 4 }} padding={1}>
+                <Accordion
+                  style={{
+                    backgroundColor: '#506ee5',
+                    border: '3px solid #506ee5',
+                    color: 'white'
+                  }}>
+                  <AccordionSummary
+                    style={{ fontSize: 20 }}
+                    aria-controls={`panel${index}-content`}
+                    id={`panel${index}-header`}
+                    expandIcon={<ExpandMoreIcon style={{ color: 'white' }} />}
+                  >
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td colSpan={2}>
+                            <h4>{stack.nome}</h4>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <small>{stack.experiencia}</small>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </AccordionSummary>
+                  <AccordionDetails style={{ padding: 10, borderTop: '1px solid white' }}>
+                    <List sx={{ width: '100%', maxWidth: 360 }} component="nav">
+                      <ListItemButton>
+                        <ListItemIcon>
+                          <Extension style={{color:'white'}} />
+                        </ListItemIcon>
+                        <ListItemText primary="Frameworks" />
+                      </ListItemButton>
+                      <List component="div" disablePadding>
+                        {
+                        (stack.frameworks === undefined) ? (
+                          <ListItemButton sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                              <ArrowRight style={{ color: 'white' }} />
+                            </ListItemIcon>
+                            <ListItemText primary='Nenhum framework conhecido' />
+                          </ListItemButton>
+                        ) :
+                        stack.frameworks.map((framework, idx) => (
+                          <ListItemButton key={idx} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                              <ArrowRight style={{ color: 'white' }} />
+                            </ListItemIcon>
+                            <ListItemText primary={framework} />
+                          </ListItemButton>
+                        ))}
+                      </List>
+                    </List>
+                  </AccordionDetails>
+                </Accordion>
+              </Grid2>
+            ))
+          }
+        </Grid2>
+      </Grid2>
+      <Grid2 size={12} padding={1} bgcolor={'#090f13'} color={'white'} paddingTop={5}>
+        <Grid2 size={12} textAlign={'center'}>
+          <Typography variant='h4'>Formações</Typography>
+        </Grid2>
+        <Grid2 container size={12} spacing={2} paddingBottom={10} marginTop={5}>
+          {
+            informacoes.formacoes.map((formacao, index) => (
+              <Grid2 key={index} size={{ xs: 12, md: 4 }} padding={1}>
+                <Accordion
+                  style={{
+                    backgroundColor: '#506ee5',
+                    border: '3px solid #506ee5',
+                    color: 'white'
+                  }}>
+                  <AccordionSummary
+                    style={{ fontSize: 20 }}
+                    aria-controls={`panel${index}-content`}
+                    id={`panel${index}-header`}
+                    expandIcon={<ExpandMoreIcon style={{ color: 'white' }} />}
+                  >
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td colSpan={2}>
+                            <h4>{formacao.curso} | {formacao.instituicao}</h4>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <small>Início: {formacao.inicio}</small>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <small>Fim: {formacao.fim}</small>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </AccordionSummary>
+                  <AccordionDetails style={{ padding: 10, borderTop: '1px solid white' }}>
+                    <List sx={{ width: '100%', maxWidth: 360 }} component="nav">
+                      <ListItemButton>
+                        <ListItemIcon>
+                          <Language style={{color:'white'}} />
+                        </ListItemIcon>
+                        <ListItemText primary="Conteúdo" />
+                      </ListItemButton>
+                      <List component="div" disablePadding>
+                        <ListItemButton sx={{ pl: 4 }}>
+                          <ListItemIcon>
+                            <ArrowRight style={{ color: 'white' }} />
+                          </ListItemIcon>
+                          <ListItemText primary={formacao.conteudo} />
+                        </ListItemButton>
+                      </List>
+                    </List>
+                  </AccordionDetails>
+                </Accordion>
+              </Grid2>
+            ))
+          }
+        </Grid2>
+      </Grid2>
+      <Grid2 size={12} padding={1} bgcolor={'#090f13'} color={'white'} paddingTop={5}>
+        <Grid2 size={12} textAlign={'center'}>
+          <Typography variant='h4'>Projetos pessoais</Typography>
+        </Grid2>
+        <Grid2 container size={12} spacing={2} paddingBottom={10} marginTop={5}>
+          {
+            informacoes.projetos.map((projeto, index) => (
+              <Grid2 key={index} size={{ xs: 12, md: 4 }} padding={1}>
+                <Accordion
+                  style={{
+                    backgroundColor: '#506ee5',
+                    border: '3px solid #506ee5',
+                    color: 'white'
+                  }}>
+                  <AccordionSummary
+                    style={{ fontSize: 20 }}
+                    aria-controls={`panel${index}-content`}
+                    id={`panel${index}-header`}
+                    expandIcon={<ExpandMoreIcon style={{ color: 'white' }} />}
+                  >
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td colSpan={2}>
+                            <h4>{projeto.nome}</h4>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </AccordionSummary>
+                  <AccordionDetails style={{ padding: 10, borderTop: '1px solid white' }}>
+                    <List sx={{ width: '100%', maxWidth: 360 }} component="nav">
+                      <ListItemButton>
+                        <ListItemIcon>
+                          <Language style={{color:'white'}} />
+                        </ListItemIcon>
+                        <ListItemText primary="Descrição" />
+                      </ListItemButton>
+                      <List component="div" disablePadding>
+                        <ListItemButton sx={{ pl: 4 }}>
+                          <ListItemIcon>
+                            <ArrowRight style={{ color: 'white' }} />
+                          </ListItemIcon>
+                          <ListItemText primary={projeto.descricao} />
+                        </ListItemButton>
+                      </List>
+                      <ListItemButton>
+                        <ListItemIcon>
+                          <Extension style={{color:'white'}} />
+                        </ListItemIcon>
+                        <ListItemText primary="Tecnologias utilizadas" />
+                      </ListItemButton>
+                      <List component="div" disablePadding>
+                        {projeto.tecnologias.map((tecnologia, idx) => (
+                          <ListItemButton key={idx} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                              <ArrowRight style={{ color: 'white' }} />
+                            </ListItemIcon>
+                            <ListItemText primary={tecnologia} />
+                          </ListItemButton>
+                        ))}
+                      </List>
+                      <ListItemButton>
+                        <ListItemIcon>
+                          <SendIcon style={{color:'white'}} />
+                        </ListItemIcon>
+                        <ListItemText primary="Link" />
+                      </ListItemButton>
+                      <List component="div" disablePadding>
+                        {
+                          projeto.link.map((link, idx) => (
+                            <ListItemButton key={idx} sx={{ pl: 4 }}>
+                              <ListItemIcon>
+                                <ArrowRight style={{ color: 'white' }} />
+                              </ListItemIcon>
+                              <ListItemText primary={<a href={link} target="_blank">Clique aqui</a>} />
+                            </ListItemButton>
+                          ))
+                        }
+                      </List> 
+                    </List>
+                  </AccordionDetails>
+                </Accordion>
+              </Grid2>
+            ))
+          }
+        </Grid2>
+      </Grid2>
+    </Grid2>
   )
 }
